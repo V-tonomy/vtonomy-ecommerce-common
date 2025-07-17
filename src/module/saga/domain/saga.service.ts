3c7bff3836cad0eacc719ba5b75123630a987064
AnChu <80581001+Chuan1404@users.noreply.github.com>
2025-07-17 22:21:09 +0700

    add saga module

---
+++ src/module/saga/domain/saga.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import {
+    CreateSagaCommand,
+    CreateSagaDTO,
+    UpdateSagaByIdCommand,
+    UpdateSagaDTO,
+} from '../core';
+import { DeleteSagaByIdCommand } from '../core/command/delete-saga-by-id.cmd';
+
+@Injectable()
+export class SagaService {
+  constructor(
+    private commandBus: CommandBus,
+    private queryBus: QueryBus,
+  ) {}
+
+  async insert(data: CreateSagaDTO) {
+    await this.commandBus.execute(CreateSagaCommand.create(data));
+  }
+
+  async updateById(id: string, data: UpdateSagaDTO) {
+    await this.commandBus.execute(UpdateSagaByIdCommand.create(id, data));
+  }
+
+  async deleteById(id: string) {
+    await this.commandBus.execute(DeleteSagaByIdCommand.create(id));
+  }
+}
--- src/module/saga/saga.module.ts
+++ src/module/saga/saga.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { SAGA_HANDLERS } from './core';
+import { SagaSchema } from './domain';
+import { SagaService } from './domain/saga.service';
 import { SagaRepository } from './infras/saga.repository';
-import { SagaController } from './infras/saga.transport';
 
 @Module({
-  controllers: [SagaController],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URL ?? 'mongodb://localhost:27017/ecommerce',
+    ),
+    MongooseModule.forFeature([{ name: 'User', schema: SagaSchema }]),
+  ],
+  controllers: [],
   providers: [
     {
       provide: 'ISagaRepository',
       useClass: SagaRepository,
     },
     ...SAGA_HANDLERS,
+    SagaService,
   ],
+  exports: [SagaService],
 })
 export class SagaModule {}
