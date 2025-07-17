b097401c8f242362dc67312d313d9de6b19eac70
AnChu <80581001+Chuan1404@users.noreply.github.com>
2025-07-17 15:10:12 +0700

    update jwt

--- src/module/jwt/jwt.module.ts
+++ src/module/jwt/jwt.module.ts
@@ -1,24 +1,40 @@
 // jwt.module.ts
-import { Global, Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { CLIENTS } from 'src/constant';
 import { JwtGuard } from './jwt.guard';
 
-@Global()
-@Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: CLIENTS.Auth_Client,
-        transport: Transport.RMQ,
-        options: {
-          urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
-          queue: 'auth_queue',
-        },
-      },
-    ]),
-  ],
-  providers: [JwtGuard],
-  exports: [JwtGuard, ClientsModule],
-})
-export class JwtModule {}
+interface JwtModuleOptions {
+  isGlobal?: boolean;
+}
+
+@Module({})
+export class JwtModule {
+  static register(options: JwtModuleOptions = {}): DynamicModule {
+    return {
+      module: JwtModule,
+      imports: [
+        ClientsModule.register([
+          {
+            name: CLIENTS.Auth_Client,
+            transport: Transport.RMQ,
+            options: {
+              urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+              queue: 'auth_queue',
+            },
+          },
+        ]),
+      ],
+      providers: options.isGlobal
+        ? [
+            {
+              provide: APP_GUARD,
+              useClass: JwtGuard,
+            },
+          ]
+        : [],
+      exports: [],
+    };
+  }
+}
